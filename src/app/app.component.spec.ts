ebd10939ceb516e3f5743a6136103483fe6387f5
Bjorn van der Neut <bvanderneut@independer.nl>
2020-10-02 11:43:10 +0200

    Change to single qoutes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,8 @@
-import { TestBed, async } from "@angular/core/testing";
-import { RouterTestingModule } from "@angular/router/testing";
-import { AppComponent } from "./app.component";
+import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
 
-describe("AppComponent", () => {
+describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [RouterTestingModule],
@@ -10,7 +10,7 @@ describe("AppComponent", () => {
     }).compileComponents();
   }));
 
-  it("should create the app", () => {
+  it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
@@ -19,19 +19,19 @@ describe("AppComponent", () => {
   it(`should have as title 'angular-jest'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual("angular-jest");
+    expect(app.title).toEqual('angular-jest');
   });
 
-  it("should render title in a h1 tag", () => {
+  it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector("h1").textContent).toContain(
-      "Welcome to angular-jest!"
+    expect(compiled.querySelector('h1').textContent).toContain(
+      'Welcome to angular-jest!'
     );
   });
 
-  it("should work with DOM types", () => {
+  it('should work with DOM types', () => {
     const fakeMouseEvent: MouseEvent = {} as MouseEvent;
     const pointerEvent: PointerEvent = {
       ...fakeMouseEvent,
